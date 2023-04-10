a6caa08fee6e1c33dd79e9b8fc83b0a8b3b1c19c
Sanjay <ksanjayk@protonmail.com>
2023-04-10 11:30:18 +0530

    array dot from

---
+++ 11_arrary_dot_from.js
@@ -0,0 +1,7 @@
+// from returns Array object from an object
+// with a length property or an iterable object
+// from = turns array-like/ish into array - string, nodeList, Set
+
+const udemy = 'udemy';
+
+console.log(Array.from(udemy));
